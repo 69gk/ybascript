37acea1067fa8499b471bc6cd9d6b60e4588508a
Guitoh <75733105+Guitoh@users.noreply.github.com>
2021-06-30 14:59:57 -0300

    Add files via upload

---
+++ ybanotifier.lua
@@ -0,0 +1,28 @@
+---SHOSEESHHHHHHH
+
+if game.PlaceId == 1537690962 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/BSS%20FREE"))()
+elseif game.PlaceId == 1499872953 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/bokubuh"))()
+elseif game.PlaceId == 5303541547 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/two%20hee"))()
+elseif game.PlaceId == 5835263912 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/OPM3"))()
+elseif game.PlaceId == 71315343 then 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/Dragonball%20Rage"))()
+elseif game.PlaceId == 6329844902 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/Last%20HEE"))()
+elseif game.PlaceId == 6574375780 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/gayaf"))()
+elseif game.PlaceId == 4520749081 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/king%20pread"))()
+elseif game.PlaceId == 6110371797 then
+setclipboard("https://discord.gg/FbSAUpGfMJ")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/ProJEctNewWorld"))()
+elseif game.PlaceId == 6688650002 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/IdkRojjutsublox"))()
+elseif game.PlaceId == 2809202155 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Historia00012/HISTORIAHUB/main/YABAAAAAAAAA"))()
+else 
+game.Players.LocalPlayer:Kick("VOCÊ CAIU NA PEGADINHA DO MARIO JOANES")
+end
